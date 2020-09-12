6bfcc2a3ac3af806d51db19238f650fd8452f755
Phong Huỳnh <p7157568@gmail.com>
2020-09-12 15:31:08 +0700

    Create skeleton structure for app

---
+++ lib/app/main/MyHomePage.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: _InstaBody(),
+      bottomNavigationBar: Container(
+
+      ),
+    );
+  }
+}
+
+class _InstaBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+
+    );
+  }
+
+}
